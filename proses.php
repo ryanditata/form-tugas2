1a5eb983b867ddbd6261cd66779d77284a8329ab
ryanditata <ryanditata38@example.com>
2025-03-10 02:48:19 +0700

    first commit

--- proses.php
+++ proses.php
@@ -5,11 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nim = $_POST['nim'];
     $nama = $_POST['nama'];
 
-    // Membuat objek Mahasiswa
     $mahasiswa = new Mahasiswa();
     $mahasiswa->setData($nim, $nama);
 
-    // Mengambil data
     $dataMahasiswa = $mahasiswa->getData();
 
     echo "<h2>Data Mahasiswa:</h2>";
